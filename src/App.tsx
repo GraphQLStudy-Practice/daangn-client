7172d19618697858068bed72bd1790f34a10c30b
lydiacho <sheecho0804@ewhain.net>
2024-06-16 19:02:12 +0900

    feat: 최소 퍼브리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import MainPage from "./views/MainPage";
 import AddPage from "./views/AddPage";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 function App() {
+  const client = new ApolloClient({
+    uri: "3.39.54.196:8080/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  client
+    .query({
+      query: gql`
+        query Products {
+          products {
+            id
+            title
+            imageUrl
+            price
+            location
+            uploadDate
+          }
+        }
+      `,
+    })
+    .then((result) => console.log(result));
+
   const router = createBrowserRouter([
     { path: "/", element: <MainPage /> },
     { path: "/add", element: <AddPage /> },
--- src/views/AddPage.tsx
+++ src/views/AddPage.tsx
@@ -1,5 +1,46 @@
+import styled from "@emotion/styled";
+
 const AddPage = () => {
-  return <div>AddPage</div>;
+  return (
+    <Container>
+      <Header>내 물건 팔기</Header>
+      <PhotoButton type="file"></PhotoButton>
+      <Title>제목</Title>
+      <Input type="text" />
+    </Container>
+  );
 };
 
 export default AddPage;
+
+const Header = styled.h1`
+  width: 100%;
+  text-align: center;
+  font-size: 20px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+
+  padding: 10px;
+
+  border: 1px solid black;
+`;
+const PhotoButton = styled.input`
+  width: 70px;
+  height: 70px;
+  border: 1px solid gray;
+  border-radius: 10px;
+`;
+
+const Title = styled.label`
+  color: black;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  border: 1px solid black;
+  border-radius: 10px;
+`;
